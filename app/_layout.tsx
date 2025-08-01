e7e40b70c8b10069cfb17a66cd339df63c228cea
chochozabe <durudduwls@gmail.com>
2025-08-01 19:31:11 +0900

    Style: prettier 설정 변경

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import "@styles/global.css";
+import '@styles/global.css';
 
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return <Stack />;
--- app/index.tsx
+++ app/index.tsx
@@ -9,9 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="text-primary">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+      <Text className="text-primary">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
