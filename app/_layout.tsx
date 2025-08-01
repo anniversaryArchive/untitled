67c7850705e933ce012cd10f7a0334480e56a0c9
chochozabe <durudduwls@gmail.com>
2025-08-01 20:01:42 +0900

    Design: 폰트 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,18 @@
-import '@styles/global.css';
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
-import { Stack } from 'expo-router';
+import "@styles/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontLoaded] = useFonts({
+    "Dunggeunmiso": require("../assets/fonts/Hakgyoansim-Dunggeunmiso-OTF-R.otf"),
+    "DunggeunmisoB": require("../assets/fonts/Hakgyoansim-Dunggeunmiso-OTF-B.otf"),
+  });
+
+  if (!fontLoaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
 }
