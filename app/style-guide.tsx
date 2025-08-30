84356ef897d6ef303d1e236210a445e94a392c05
KimHyejin <hyepago@gmail.com>
2025-08-30 22:54:18 +0900

    Feat: WiggleBox 컴포넌트 추가 (#6)
    * Feat. WiggleBox component 추가
    
    * mod. 필요없는 코드 제거
    
    * mod. WiggleBorder type > interface
    
    * mod. WiggleBorder - backgroundColor 지정 가능, frequency default set 100, box 크기 조정
    
    * Fix: wiggleborder styleguide 복구
    
    ---------
    
    Co-authored-by: Heidi <heidi@worksmate.co.kr>
    Co-authored-by: Kim Yeo Kyoung <39088247+chochozabe@users.noreply.github.com>
    Co-authored-by: chochozabe <durudduwls@gmail.com>

--- app/style-guide.tsx
+++ app/style-guide.tsx
@@ -1,5 +1,5 @@
-import { ScrollView, View } from "react-native";
-import { Typography, Button, InputBox, TextBox } from "@/components";
+import { Image, ScrollView, View } from "react-native";
+import { Typography, Button, InputBox, TextBox, WiggleBorder } from "@/components";
 
 export default function StyleGuide() {
   return (
@@ -223,6 +223,98 @@ export default function StyleGuide() {
             </Typography>
             <View className="grow flex gap-2">
               <TextBox bold placeholder="bold=true" />
+            </View>
+          </View>
+          <View className="flex gap-4">
+            <Typography
+              variant="Header2"
+              color="primary"
+              className="bg-primary-light border-primary-light p-1 border"
+            >
+              WiggleBorder
+            </Typography>
+            <View className="flex flex-row gap-5">
+              <Typography variant="Header3" color="secondary" className="w-[100px]">
+                Default
+              </Typography>
+              <View className="flex-1">
+                <WiggleBorder>
+                  <View className="p-2">
+                    <Typography variant="Body2" className="text-center">
+                      Default Wiggle Border
+                    </Typography>
+                  </View>
+                </WiggleBorder>
+              </View>
+            </View>
+            <View className="flex flex-row gap-5">
+              <Typography variant="Header3" color="secondary" className="w-[100px]">
+                Colors
+              </Typography>
+              <View className="flex-1">
+                <WiggleBorder strokeColor="secondary">
+                  <View className="p-2">
+                    <Typography variant="Body2" className="text-center">
+                      Secondary Wiggle Border
+                    </Typography>
+                  </View>
+                </WiggleBorder>
+
+                <WiggleBorder strokeColor="secondary.dark">
+                  <View className="p-2">
+                    <Typography variant="Body2" className="text-center">
+                      Secondary Dark Wiggle Border
+                    </Typography>
+                    <Typography variant="Body2" className="text-center">
+                      Tailwind 에 명시한 컬러 모두 가능 ~
+                    </Typography>
+                  </View>
+                </WiggleBorder>
+
+                <WiggleBorder strokeColor="#ccaaaa">
+                  <View className="p-2">
+                    <Typography variant="Body2" className="text-center">
+                      Hex 컬러도 당연히 가능함둥
+                    </Typography>
+                  </View>
+                </WiggleBorder>
+              </View>
+            </View>
+            <View className="flex flex-row gap-5">
+              <Typography variant="Header3" color="secondary" className="w-[100px]">
+                Wiggle
+              </Typography>
+              <View className="flex-1">
+                <WiggleBorder wiggle={100}>
+                  <View className="p-2">
+                    <Typography variant="Body2" className="text-center">
+                      Wiggle 100
+                    </Typography>
+                    <Typography variant="Body2" className="text-center">
+                      Tailwind 에 명시한 컬러 모두 가능 ~
+                    </Typography>
+                  </View>
+                </WiggleBorder>
+
+                <WiggleBorder smoothen={100}>
+                  <View className="p-2">
+                    <Typography variant="Body2" className="text-center">
+                      smoothen 100
+                    </Typography>
+                  </View>
+                </WiggleBorder>
+
+                <View className="w-52 h-52">
+                  <WiggleBorder frequency={100} backgroundColor="primary">
+                    <Image
+                      className="w-48 h-48"
+                      source={{
+                        uri: "https://bandai-a.akamaihd.net/bc/img/model/b/1000234207_1.jpg",
+                      }}
+                    />
+                  </WiggleBorder>
+                </View>
+              </View>
             </View>
           </View>
         </View>
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Typography from "./Typography";
 import Button from "./Button";
 import { InputBox, TextBox } from "./Input";
+import WiggleBorder from "./WiggleBorder";
 
-export { Typography, Button, InputBox, TextBox };
+export { Typography, Button, InputBox, TextBox, WiggleBorder };
