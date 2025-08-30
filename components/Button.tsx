79c1d47832e8211848b0d0f4ae2489660ade0cd8
chochozabe <durudduwls@gmail.com>
2025-08-30 23:47:38 +0900

    Mod: button 수정

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,9 +3,9 @@ import { Pressable, Text } from "react-native";
 
 interface IButtonProps {
   children?: React.ReactNode;
-  color?: "primary" | "secondary" | "secondary-dark";
+  color?: keyof typeof buttonTheme.color;
   width?: "auto" | "full";
-  size?: "sm" | "md" | "lg";
+  size?: keyof typeof buttonTheme.size;
   variant?: "contained" | "outlined" | "text";
   rounded?: boolean;
   disabled?: boolean;
@@ -13,27 +13,31 @@ interface IButtonProps {
 }
 
 const buttonTheme = {
-  primary: {
-    contained: "bg-primary border-primary text-secondary-light border",
-    outlined: "border-primary text-primary border-2",
-    text: "text-primary",
-    disabled: "bg-gray-300 border-gray-300 text-primary-light-80 border",
+  color: {
+    primary: {
+      contained: "bg-primary border-primary text-secondary-light border",
+      outlined: "border-primary text-primary border-2",
+      text: "text-primary",
+      disabled: "bg-gray-300 border-gray-300 text-primary-light-80 border",
+    },
+    secondary: {
+      contained: "bg-secondary border-secondary text-secondary-light border",
+      outlined: "border-secondary text-secondary border-2",
+      text: "text-secondary",
+      disabled: "bg-gray-300 border-gray-300 text-secondary-light-80 border",
+    },
+    "secondary-dark": {
+      contained: "bg-secondary-light border-secondary-light text-secondary-dark border",
+      outlined: "border-secondary-dark text-secondary-dark border-2",
+      text: "text-secondary-dark",
+      disabled: "bg-gray-300 border-gray-300 text-secondary-dark-80 border",
+    },
   },
-  secondary: {
-    contained: "bg-secondary border-secondary text-secondary-dark border",
-    outlined: "border-secondary text-secondary border-2",
-    text: "text-secondary",
-    disabled: "bg-gray-300 border-gray-300 text-secondary-light-80 border",
+  size: {
+    sm: "h-[28px] font-DunggeunmisoB text-[14px] py-[6.5px] px-[7.5px]",
+    md: "h-[30px] font-Dunggeunmiso text-[14px] py-2 px-3",
+    lg: "h-[44px] font-DunggeunmisoB text-[17px] py-[14px] px-[15px]",
   },
-  "secondary-dark": {
-    contained: "bg-secondary-light border-secondary-light text-secondary-dark border",
-    outlined: "border-secondary-dark text-secondary-dark border-2",
-    text: "text-secondary-dark",
-    disabled: "bg-gray-300 border-gray-300 text-secondary-dark-80 border",
-  },
-  sm: "h-[28px] font-DunggeunmisoB text-[14px] py-[6.5px] px-[7.5px]",
-  md: "h-[30px] font-Dunggeunmiso text-[14px] py-2 px-3",
-  lg: "h-[44px] font-DunggeunmisoB text-[17px] py-[14px] px-[15px]",
 };
 
 const Button = (props: IButtonProps) => {
@@ -56,7 +60,7 @@ const Button = (props: IButtonProps) => {
     <Pressable className={`${width == "full" && "grow"}`} {...options}>
       {children && (
         <Text
-          className={`${defaultStyle} ${widthProps} ${borderRadiusProps} ${disabled ? buttonTheme[color].disabled : buttonTheme[color][variant]} ${buttonTheme[size]}`}
+          className={`${defaultStyle} ${widthProps} ${borderRadiusProps} ${disabled ? buttonTheme.color[color].disabled : buttonTheme.color[color][variant]} ${buttonTheme.size[size]}`}
         >
           {children}
         </Text>
