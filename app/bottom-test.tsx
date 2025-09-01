015766a9077582eed7dfd3db6a0c6176fd8d1a1c
sjkim <sojungkim318@gmail.com>
2025-09-01 11:37:08 +0900

    Fix : GestureHandlerRootView 루트 제스처 오류 수정

--- app/bottom-test.tsx
+++ app/bottom-test.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-import { View, Button } from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 import BottomSheet from "../components/BottomSheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function BottomTest() {
   const [open, setOpen] = useState(false);
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Button className="font-dunggeunmiso text-secondary-dark text-3xl" title="Open BottomSheet" onPress={() => setOpen(true)} />
-      <BottomSheet open={open} onClose={() => setOpen(false)} />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <View className="flex-1 items-center justify-center">
+        <Button
+          className="font-dunggeunmiso text-secondary-dark text-3xl"
+          title="Open BottomSheet"
+          onPress={() => setOpen(true)}
+        />
+        <BottomSheet open={open} onClose={() => setOpen(false)} />
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
