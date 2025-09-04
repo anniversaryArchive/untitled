c9dda461b83151f3a5252dd1afc372f3b065b175
Kim Yeo Kyoung <39088247+chochozabe@users.noreply.github.com>
2025-09-04 15:48:41 +0900

    Feat: Chip 컴포넌트 추가 (#10)
    * Feat: Chip 컴포넌트 추가
    
    * Mod: 컴포넌트 재정의 방지

--- components/index.ts
+++ components/index.ts
@@ -3,5 +3,6 @@ import Button from "./Button";
 import { InputBox, TextBox } from "./Input";
 import WiggleBorder from "./WiggleBorder";
 import ProgressBar from "./ProgressBar";
+import Chip from "./Chip";
 
-export { Typography, Button, InputBox, TextBox, WiggleBorder, ProgressBar };
+export { Typography, Button, InputBox, TextBox, WiggleBorder, ProgressBar, Chip };
