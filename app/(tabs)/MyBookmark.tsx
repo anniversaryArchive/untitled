8f8913e338d2ad83635bc5451f6780775576a976
Kim Yeo Kyoung <39088247+chochozabe@users.noreply.github.com>
2025-09-19 12:28:33 +0900

    Feat: 내 굿즈 페이지 추가 (#20)
    * Feat: 폴더 선택 컴포넌트 추가
    
    * Refactor: type 수정
    
    * Fix: 태그 짝 맞추기
    
    * Mod: 띄어쓰기 추가
    
    * Mod: bold 시 contained border width 수정
    
    * Mod: Button 컴포넌트 수정
    
    * Refactor: import문 정리
    
    * Fix: bottomSheet 종료 시 키보드 비활성화
    
    * Feat: 내 굿즈 페이지 추가
    
    * Fix: 순환 종속성 수정
    
    * Refactor: 폴더 테이블 folder → folders 로 변경
    
    * Mod: 폴더 테이블명 변경 반영
    
    * Mod: PR 코멘트 반영

--- app/(tabs)/MyBookmark.tsx
+++ app/(tabs)/MyBookmark.tsx
@@ -1,12 +1,119 @@
-import { View } from "react-native";
-import Typography from "@/components/Typography";
+import { useEffect, useState } from "react";
+import { FlatList, Pressable, ScrollView, View } from "react-native";
+
+import { Button, GoodsThumbnail, Icon, InputBox, Segment, Typography } from "@components/index";
+import { colors } from "@utils/tailwind-colors";
+import { BOOKMARK_TYPE } from "@/constants/global";
+import folder from "@table/folders";
+
+import { TFolder } from "@/types/folder";
+import { TBookmarkType } from "@/types/bookmark";
 
 export default function MyBookmark() {
+  const [mode, setMode] = useState<TBookmarkType>("WISH");
+  const [viewMode, setViewMode] = useState<"folder" | "item">("item");
+  const [folderList, setFolderList] = useState<TFolder[]>([]);
+  const [selectedFolder, setSelectedFolder] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const loadFolderList = async () => {
+      const folderList = await folder.getAll();
+      setFolderList([{ id: 0, name: "전체", created_at: new Date() }, ...folderList]);
+    };
+
+    loadFolderList();
+  }, []);
+
+  useEffect(() => {
+    setSearchTerm("");
+    setViewMode("item");
+    setSelectedFolder(0);
+  }, [mode]);
+
   return (
-    <View>
-      <Typography variant="Header1" color="secondary-dark">
-        내 굿즈
-      </Typography>
+    <View className="flex-1 gap-4 px-6 pt-1">
+      {/* Header */}
+      <View className="flex flex-row items-center justify-between">
+        <Typography variant="Header1" color="primary">
+          LOGO
+        </Typography>
+        <Pressable>
+          <Icon name="search" size={32} fill={colors.primary.DEFAULT} />
+        </Pressable>
+      </View>
+
+      <Segment segments={BOOKMARK_TYPE} selectedKey={mode} onSelect={setMode} />
+      <View className="flex-1 gap-4">
+        {/* 폴더 리스트 */}
+        <ScrollView
+          horizontal
+          className="min-h-8 flex-none -mx-6"
+          contentContainerClassName="flex gap-3 flex-row pl-6"
+          showsHorizontalScrollIndicator={false}
+        >
+          {folderList.map(({ id, name }) => {
+            const isSelected = id === selectedFolder;
+
+            return (
+              <Button
+                key={id}
+                bold
+                variant={isSelected ? "contained" : "outlined"}
+                onPress={() => {
+                  setSelectedFolder(id);
+                }}
+              >
+                {name}
+              </Button>
+            );
+          })}
+        </ScrollView>
+        <InputBox size="md" color="secondary" value={searchTerm} onChangeText={setSearchTerm} />
+
+        {/* WISH | GET */}
+        <View className="flex-1 gap-1">
+          <View className="flex-row items-center justify-end gap-1">
+            <Button
+              bold
+              variant="text"
+              color={viewMode === "folder" ? "primary" : "secondary-dark"}
+              contentClassName={viewMode === "folder" ? "" : "text-secondary-dark-80"}
+              onPress={() => {
+                setViewMode("folder");
+              }}
+            >
+              폴더 보기
+            </Button>
+            <Button
+              variant="text"
+              bold
+              color={viewMode === "item" ? "primary" : "secondary-dark"}
+              contentClassName={viewMode === "item" ? "" : "text-secondary-dark-80"}
+              onPress={() => {
+                setViewMode("item");
+              }}
+            >
+              아이템 보기
+            </Button>
+          </View>
+          <FlatList
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
+            contentContainerClassName="pb-4"
+            columnWrapperClassName="flex flex-row justify-between"
+            numColumns={2}
+            keyExtractor={(_, index) => `${index}`}
+            showsVerticalScrollIndicator={false}
+            renderItem={() => {
+              return (
+                <View className="p-2">
+                  <GoodsThumbnail />
+                </View>
+              );
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
